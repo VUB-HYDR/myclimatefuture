491e095decb2e98de6489611799e203d6976ca51
Jonas Parnow <6gqrbj@posteo.org>
2024-03-27 16:31:39 +0100

    Clean up

--- src/lib/DownloadButton/DownloadButton.svelte
+++ src/lib/DownloadButton/DownloadButton.svelte
@@ -7,29 +7,16 @@
   import { onMount } from 'svelte';
   import Download from '../Icons/Download.svelte';
 
-  let file;
-  IMAGE_FILE_NAME.subscribe((value) => {
-    file = value;
-  });
-  let width;
-  CURRENT_VIS_WIDTH.subscribe((value) => {
-    width = value;
-  });
-  let height;
-  CURRENT_VIS_HEIGHT.subscribe((value) => {
-    height = value;
-  });
-
   // The option to NOT actually save the image is for the automatic call in mount further down.
   function getGraphAsPNG(shouldSave = true) {
     const div = document.getElementById(ID_GRAPH);
     if (div) {
-      htmlToImage.toBlob(div, { backgroundColor: '#ffffff', canvasWidth: width * 2, canvasHeight: height * 2 }).then((blob) => {
+      htmlToImage.toBlob(div, { backgroundColor: '#ffffff', canvasWidth: $CURRENT_VIS_WIDTH * 2, canvasHeight: $CURRENT_VIS_HEIGHT * 2 }).then((blob) => {
         if (shouldSave) {
           if (window.saveAs) {
-            window.saveAs(blob, file);
+            window.saveAs(blob, $IMAGE_FILE_NAME);
           } else {
-            saveAs(blob, file);
+            saveAs(blob, $IMAGE_FILE_NAME);
           }
         }
       });
@@ -45,7 +32,13 @@
   });
 </script>
 
-<button type="button" class="download-btn" on:click={getGraphAsPNG} aria-label={$t('content.BUTTON_DOWNLOAD_TITLE')} title={$t('content.BUTTON_DOWNLOAD_TITLE')}>
+<button
+  type="button"
+  class="download-btn p-2 bg-accent transition-colors text-white rounded-md flex items-center justify-center gap-x-4 hover:bg-primary"
+  on:click={getGraphAsPNG}
+  aria-label={$t('content.BUTTON_DOWNLOAD_TITLE')}
+  title={$t('content.BUTTON_DOWNLOAD_TITLE')}
+>
   <span class="label">{$t('content.BUTTON_DOWNLOAD')}</span>
   <Download />
 </button>
@@ -54,25 +47,6 @@
   @import '../../styles/global.scss';
 
   button {
-    @include button-reset();
-    transition: background-color var(--transition);
-    background-color: var(--color-accent);
-    color: var(--text-color-invert);
-    padding: 0.5rem;
-    border-radius: var(--border-radius);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    .label {
-      margin-right: var(--spacing-1);
-    }
-
-    &:hover,
-    &:focus {
-      background-color: var(--primary-color);
-    }
-
     @include query($medium) {
       grid-column: 2 / span 2;
     }
