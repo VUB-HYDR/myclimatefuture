6e31d68fdc21f81643db58fed5f6f62286fed05a
Jonas Parnow <6gqrbj@posteo.org>
2024-04-09 11:30:50 +0200

    Netlify settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import { preprocessMeltUI, sequence } from '@melt-ui/pp';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
   kit: {
